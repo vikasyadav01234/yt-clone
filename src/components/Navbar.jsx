29bdf4ee37e50c76338d9f592a942b152b6612cf
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-11-11 16:36:02 +0530

    "Added react-icons dependency, updated App.jsx to import Navbar component, and removed existing App component code"

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+function Navbar(){
+    return (
+        <div className="flex justify-between px-6 py-2">
+            <div className="border border-black">
+                1
+            </div>
+            <div className="border border-black">
+                2
+            </div>
+            <div className="border border-black">
+                3
+            </div>
+        </div>
+    )
+}
+export default Navbar
