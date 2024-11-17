bc486adc6f406b7511a98a1592e271663e49c049
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-11-17 11:15:47 +0530

    "Added react-avatar dependency and updated Navbar component to use Avatar from react-avatar"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Avatar from 'react-avatar';
 import {AiOutlineMenu} from "react-icons/ai";
 import logo from "../assets/logo.png";
 import {CiSearch} from "react-icons/ci";
@@ -10,11 +11,11 @@ import profile from "../assets/profile.jpg"
 function Navbar(){
     return (
         <div className="flex justify-between px-6 py-2">
-            <div className="flex border border-black items-center space-x-4">
+            <div className="flex items-center space-x-4">
                 <AiOutlineMenu className='text-xl cursor-pointer'/>
                 <img src={logo} alt="" className="w-28 cursor-pointer"/>
             </div>
-            <div className="border w-[35%] border-black flex">
+            <div className="w-[35%] flex items-center">
                 <div className='w-[100%] px-3 py-2 border rounded-l-full'>
                 <input type="text" placeholder='Search' className="outline-none"/>
                 </div>
@@ -25,10 +26,10 @@ function Navbar(){
                 size={"42px"}
                 className='ml-3 border rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200'/>
             </div>
-            <div className="border border-black">
+            <div className="flex space-x-5 items-center">
                 <RiVideoAddLine/>
                 <AiOutlineBell/>
-                <img src={profile} alt="" className="w-12 rounded-full ml-3"/>
+                <Avatar src={profile} size='32' round={true}/>
             </div>
         </div>
     );
